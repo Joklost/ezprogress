24ffeb9a78ee55df97d80c50bb9e650e8a85045e
Jonas Kloster Jacobsen <joklost@gmail.com>
2018-02-22 10:04:13 +0100

    Update license

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name='ezprogress',
-    version='1.0',
+    version='1.0.0',
     description='Simple, easy to use progress bar.',
     author='Jonas Kloster Jacobsen',
     author_email='joklost@gmail.com',
-    license='GNU',
+    license='GPL-3.0',
     packages=['ezprogress'],
     url='https://github.com/Joklost/ezprogress',
     zip_safe=False
